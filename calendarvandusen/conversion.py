1722b2e0ad4b68dccab3715e8e1ec793888097da
StigHaraldGustavsen <stighg@gmail.com>
2023-03-06 08:12:36 +0100

    added convert to lating and did a convert loop

--- calendarvandusen/conversion.py
+++ calendarvandusen/conversion.py
@@ -10,6 +10,14 @@ def CVD_to_greek(A, B, C=0.0):
     
     return res 
 
+def CVD_to_latin(alpha, delta, beta=0.0):
+    A = alpha+((alpha*delta)/100)
+    B = -((alpha*delta)/(100**2))
+    C = -((alpha*beta)/(109**4))
+    
+    res = {'A':A,'B':B, 'C':C} 
+    return res 
+
 if __name__ == '__main__':
     print('A = 3.9083e-3, B:-5.775e-7 C:-4.183e-12')
     print(CVD_to_greek(3.9083e-3,-5.775e-7,-4.183e-12))
@@ -27,7 +35,9 @@ if __name__ == '__main__':
     poly = R0*(1+((A*t)+(B*t**2)))
     
     #getting the greek coefisient
-    abg = CVD_to_greek(A,B)
+    print('Referance : A:'+str(A)+' B:'+str(B)+' C='+str(C))
+    abg = CVD_to_greek(A,B,C)
+    print('converted to: '+str(abg))
 
     #Calculate with the greek wierd polynomial.
     t_abg = R0+R0*abg['alpha']*(t-abg['delta']*((t/100)-1)*(t/100)-abg['beta']*((t/100)-1)*((t/100)**3))
@@ -36,4 +46,22 @@ if __name__ == '__main__':
     sse = 0 
     for i in range(len(t)):
         sse += (poly[i]-t_abg[i])**2
-    print(sse)
+    print('Sum square error: '+str(sse))
+
+    #covert back
+    
+    print('convert back ')
+    
+    abc = CVD_to_latin(abg['alpha'],abg['delta'],abg['beta'])
+    t_abc = R0*(1+((abc['A']*t)+(abc['B']*t**2)))
+    print(abg)
+    
+    print('Referance : A:'+str(A)+' B:'+str(B)+' C='+str(C))
+    print(abc) 
+    
+
+    #Calculating the sum of the squared errors of points from -10°C to 50°C
+    sse = 0 
+    for i in range(len(t)):
+        sse += (t_abg[i]-t_abc[i])**2
+    print('Sum square error: '+str(sse))
